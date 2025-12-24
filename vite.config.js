4854a0262d780b5eaa60b0d4e847b2a557e6af3c
Mayur <mayekarmayur1122@gmail.com>
2025-12-24 12:14:12 +0530

    Complete sync

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Bill-Scanner/',
+  //base: '/Bill-Scanner/',
   plugins: [
     react(),
     tailwindcss(),
